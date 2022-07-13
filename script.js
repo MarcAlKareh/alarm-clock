8739c889abe8b58fa6fbde64641b19055ab947de
MarcAlKareh <ma7748@student.st-chris.net>
2022-07-13 16:42:59 +1000

    Got time in hours:minutes:seconds

--- script.js
+++ script.js
@@ -3,29 +3,41 @@ const timerEl = document.querySelector('.timer');
 const minutesEl = document.querySelector('.minutes');
 
 class App {
-  #timer;
-
   constructor() {
-    timerForm.addEventListener('submit', this.startTimer.bind(this));
+    timerForm.addEventListener('submit', this._startTimer.bind(this));
   }
 
-  startTimer(e) {
+  _getCountdownTime(inputMinutes) {
+    const seconds = inputMinutes * 60;
+
+    // Get hours, minutes and seconds
+    const hours = String(Math.floor(inputMinutes / 60)).padStart(2, 0);
+    const mins = String(inputMinutes % 60).padStart(2, 0);
+    const secs = String(seconds % 60).padStart(2, 0);
+
+    return [hours, mins, secs];
+  }
+
+  _startTimer(e) {
     e.preventDefault();
+
+    // Clear input box
+    minutesEl.value = '';
 
     const validMinutes = mins => Number.isFinite(mins);
     const positiveMins = mins => mins > 0;
 
-    const minutes = minutesEl.value;
+    let inputMinutes = minutesEl.value;
 
     // Validate user input for minutes
-    if (!validMinutes(minutes) && !positiveMins(minutes)) {
-      minutesEl.value = '';
+    if (!validMinutes(inputMinutes) && !positiveMins(inputMinutes)) {
       return alert('Minutes have to be a positive number');
     }
 
-    minutesEl.value = '';
+    const [hours, mins, secs] = this._getCountdownTime(inputMinutes);
+    console.log(hours, mins, secs);
 
-    timerEl.textContent = minutes;
+    timerEl.textContent = `${hours}:${mins}:${secs}`;
   }
 }
 
